d1558bdd39a7ddb80e12efed1bb1efa9f44bd041
RemAdmin <pl712993@gmail.com>
2021-06-03 09:16:41 +0500

    Initial commit

---
+++ src/StateObject.java
@@ -0,0 +1,18 @@
+public class StateObject {
+    private int x = 0;
+    private int count = 0;
+    public int getX(){
+        return x;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void incrementX() {
+        x++;
+    }
+    public void incrementCount(){
+        count++;
+    }
+}
